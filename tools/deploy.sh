381361553f4262c110c69fe9b550a6ec1b3dd4c6
sleticalboy <sleticalboy@gmail.com>
2020-12-05 16:24:05 +0800

    add logs to tools/deploy.sh.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -14,6 +14,8 @@ PAGES_BRANCH="gh-pages"
 _no_branch=false
 _backup_dir="$(mktemp -d)"
 
+echo "backup dir: $_backup_dir"
+
 init() {
   if [[ -z $(git branch -av | grep "$PAGES_BRANCH") ]]; then
     _no_branch=true
