af995b4f006dbfb35a7e447c488ed707a52b10b2
sleticalboy <sleticalboy@gmail.com>
2020-12-08 10:10:31 +0800

    update publish scripgs

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #
-# Deploy the content of _site to 'origin/<pages_branch>'
-#
-# v2.5
-# https://github.com/cotes2020/jekyll-theme-chirpy
-# © 2020 Cotes Chung
-# Published under MIT License
-
-set -eu
+# Deploy the content of _site to 'origin/gh-pages'
 
 main() {
     work_dir=`mktemp -d`
@@ -17,22 +10,42 @@ main() {
     cd $work_dir
     cp -r $cur_dir/.git* .
     
+    echo "git restore . && git pull --rebase"
     git restore .
     git pull --rebase
-    
-    msg=`git checkout gh-pages | grep "git pull"`
-    if [[ $msg ]]; then
-        echo "sync with git pull --rebase"
-        git pull --rebase >>/dev/null
+
+    _init=false
+
+    msg=`git branch -av | grep gh-pages`
+    # [[ -z $msg ]] 字符串 msg 为空
+    # [[ $msg ]] 字符串 msg 非空
+    if [[ -z $msg ]]; then
+        _init=true
+        echo "git checkout -b gh-pages"
+        git checkout -b gh-pages
+    else
+        _init=false
+        echo 'git checkout gh-pages | grep "git pull"'
+        msg=`git checkout gh-pages | grep "git pull"`
+        if [[ $msg ]]; then
+            echo "sync with git pull --rebase"
+            git pull --rebase >>/dev/null
+        fi
     fi
     
     cp -r $cur_dir/_site/* .
     git add . && git commit -m "published on `date`"
     
-    msg=`git status | grep "git push"`
-    if [[ "$msg" ]]; then
-        echo "deploy with git push"
-        git push
+    if $_init; then
+        echo 'git push -u origin gh-pages'
+        git push -u origin gh-pages
+    else
+        echo 'git status | grep "git push"'
+        msg=`git status | grep "git push"`
+        if [[ "$msg" ]]; then
+            echo "deploy with git push"
+            git push
+        fi
     fi
 
     rm -rf "$work_dir"
